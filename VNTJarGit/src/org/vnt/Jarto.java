9c4b3f063bb51c4aa2097613287b376e7987d9bd
vntran <nghia.tranv@gmail.com>
2013-04-05 14:40:17 +0200

    Add class

---
+++ VNTJarGit/src/org/vnt/Jarto.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vnt;
+
+/**
+ * @author VNT
+ * @Date 5 Apr 2013
+ */
+public class Jarto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
