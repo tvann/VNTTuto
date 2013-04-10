e50e223db1e60a1c05724415e241d132c77badb9
vntran <nghia.tranv@gmail.com>
2013-04-10 18:23:55 +0200

    Add Tomtom

---
+++ VNTWebGit/src/org/vnt/Tomtom.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vnt;
+
+/**
+ * @author VNT
+ * @Date 10 Apr 2013
+ */
+public class Tomtom {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
